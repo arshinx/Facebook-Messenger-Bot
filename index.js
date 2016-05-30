14c0b2f1eecfc221e4b1e929431e7d86ef501b6f
Arshin Jain <arshin.jain@outlook.com>
2016-05-29 22:03:03 -0700

    updated the bot to speak

--- index.js
+++ index.js
@@ -30,3 +30,37 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAPoceZAUeZCQBAAMZBzYyssc189xY77ijQXXmUiZBSYioVEZA5ZC1okL7zX04JOM7Ukdhh8nJePs4iZBfIDLaX1u7TRi0qM9zoWh0ZBtCjMhzd1oPFDnrZCXHEZANhKSkj3hoYTJTEjoUT7wENJoECpxO37HyC7qwAumb4LmiTNLy2wZDZD"
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
